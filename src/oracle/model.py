d1df03d7933cb4d4fcb8948da976f582ff8b8d4d
Payam Yektamaram <payamyek@gmail.com>
2025-02-26 23:25:08 -0500

    feat: add age upper bound

--- src/oracle/model.py
+++ src/oracle/model.py
@@ -3,8 +3,14 @@ from typing import Literal
 import pandas as pd
 from pydantic import PositiveInt
 
+# we cant estimate beyond this age
+AGE_UPPER_BOUND = 110
+
 
 def predict_life_expectancy(age: PositiveInt, sex: Literal["M", "F"]) -> int:
+    if age > AGE_UPPER_BOUND:
+        return age
+
     # get life table based on sex
     life_table = (
         pd.read_pickle("data/canada_male_life_table.pkl")
@@ -16,3 +22,6 @@ def predict_life_expectancy(age: PositiveInt, sex: Literal["M", "F"]) -> int:
     life_expectancy_at_age = life_table.query("age == @age")["e_x"].item()
 
     return int(age + life_expectancy_at_age)
+
+
+print(predict_life_expectancy(age=111, sex="M"))

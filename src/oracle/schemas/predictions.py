3c95d1a22b0fdfc6bb50e5f6a6fb62395e3e311b
Payam Yektamaram <payamyek@gmail.com>
2025-02-28 21:46:14 -0500

    feat: add computed field `age`

--- src/oracle/schemas/predictions.py
+++ src/oracle/schemas/predictions.py
@@ -1,12 +1,19 @@
+from datetime import date
+from functools import cached_property
 from typing import Literal
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field, computed_field
 
 
 class Prediction(BaseModel):
-    age: int
+    date_of_birth: date
     sex: Literal["M", "F"]
+
+    @computed_field
+    @cached_property
+    def age(self) -> int:
+        return int((date.today() - self.date_of_birth).days / 365)
 
 
 class PredictionResponse(BaseModel):
-    life_expectancy: int
+    life_expectancy: int = Field(ge=0)

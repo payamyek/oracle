e9bf7c46ae421cf10c7c243c29f70e8d67271337
Payam Yektamaram <payamyek@gmail.com>
2025-03-02 14:15:41 -0500

    feat: add healthcehck

--- src/oracle/main.py
+++ src/oracle/main.py
@@ -6,4 +6,11 @@ from oracle.routers import predictions
 log.info("Application starting up ...")
 
 app = FastAPI()
+
+
+@app.get("/health")
+def health_check():
+    return {"status": "healthy"}
+
+
 app.include_router(router=predictions.router)

78410b9724822b7db583ddaff0a289d9d2d3eba6
Payam Yektamaram <payamyek@gmail.com>
2025-03-11 22:38:36 -0400

    feat: support psycopg2 in dockerfile

--- src/oracle/config.py
+++ src/oracle/config.py
@@ -3,7 +3,5 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
-    database_url: str = Field(
-        default="postgresql://postgres:ZQDcCsqgURHDDIRLSGKldnylxKTrHJVS@crossover.proxy.rlwy.net:53744/railway"
-    )
+    database_url: str = Field(default="")
     model_config = SettingsConfigDict(env_prefix="ORACLE_")

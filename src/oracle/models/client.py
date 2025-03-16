9b2c66970ebdd4b690788d652caea9fcc6819912
Payam Yektamaram <payamyek@gmail.com>
2025-03-16 11:22:48 -0400

    refactor: clients api models

--- src/oracle/models/client.py
+++ src/oracle/models/client.py
@@ -8,10 +8,20 @@ from sqlmodel import Field, SQLModel
 class ClientBase(SQLModel):
     first_name: str
     last_name: str
+    email: EmailStr = Field(unique=True)
 
 
 class Client(ClientBase, table=True):
     id: uuid.UUID = Field(primary_key=True, default_factory=uuid.uuid4)
-    email: EmailStr = Field(unique=True)
     is_verified: bool = False
     created_at: datetime = datetime.now()
+
+
+class ClientCreate(ClientBase):
+    pass
+
+
+class ClientPublic(ClientBase):
+    id: uuid.UUID
+    is_verified: bool
+    created_at: datetime
--- src/oracle/routers/client.py
+++ src/oracle/routers/client.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter, HTTPException
 from sqlmodel import select
 
 from oracle.dependencies import SessionDep
-from oracle.models.client import Client
+from oracle.models.client import Client, ClientCreate, ClientPublic
 
 router = APIRouter(
     prefix="/api/v1/clients",
@@ -10,7 +10,7 @@ router = APIRouter(
 
 
 @router.post("")
-def create_client(client: Client, session: SessionDep) -> Client:
+def create_client(client: ClientCreate, session: SessionDep) -> ClientPublic:
     db_client = Client.model_validate(client)
 
     statement = select(Client).where(Client.email == client.email)
@@ -22,4 +22,4 @@ def create_client(client: Client, session: SessionDep) -> Client:
     session.add(db_client)
     session.commit()
     session.refresh(db_client)
-    return db_client
+    return ClientPublic.model_validate(db_client)

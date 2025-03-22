9f93552e237a5ec253e5759c82f3c2932255d727
Payam Yektamaram <payamyek@gmail.com>
2025-03-22 11:27:54 -0400

    feat: smoking impact

--- src/oracle/models/prediction.py
+++ src/oracle/models/prediction.py
@@ -1,8 +1,8 @@
 from datetime import date, datetime
 from functools import cached_property
-from typing import Literal
+from typing import Literal, Optional, Self
 
-from pydantic import BaseModel, Field, computed_field
+from pydantic import BaseModel, Field, computed_field, model_validator
 
 
 def _calculate_age(date_of_birth: date) -> int:
@@ -15,21 +15,39 @@ class PredictionCreate(BaseModel):
     sex: Literal["M", "F"]
     country_code: Literal["CA"] = Field(description="Country code adhering to ISO 3166-1 alpha-2")
 
+    smoking_start_date: Optional[date] = None
+    smoking_end_date: Optional[date] = None
+    smoking_daily_frequency: Optional[int] = None
+
     @computed_field
     @cached_property
     def age(self) -> int:
         return _calculate_age(self.date_of_birth)
 
+    @model_validator(mode="after")
+    def check_smoking_data(self) -> Self:
+        if self.smoking_start_date is not None and self.smoking_daily_frequency is None:
+            raise ValueError("Smoking data is meaningless without the frequency")
+        elif self.smoking_start_date is None and self.smoking_end_date is not None:
+            raise ValueError("Smoking time period with end dates must have a start date")
+        elif (
+            self.smoking_start_date is not None
+            and self.smoking_end_date is not None
+            and self.smoking_end_date <= self.smoking_start_date
+        ):
+            raise ValueError("Smoking start date must precede the end date")
+        return self
+
 
 class PredictionPublic(BaseModel):
-    life_expectancy: int = Field(ge=0)
+    life_expectancy: float = Field(ge=0)
     date_of_birth: date
 
     @computed_field
     @cached_property
     def date_of_death(self) -> date:
         age = _calculate_age(self.date_of_birth)
-        return date(self.life_expectancy - age + date.today().year, 1, 1)
+        return date(int(self.life_expectancy - age + date.today().year), 1, 1)
 
     @computed_field
     @cached_property
--- src/oracle/services/predictor.py
+++ src/oracle/services/predictor.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 import pandas as pd
 
 from oracle.models.prediction import PredictionCreate
@@ -5,19 +7,31 @@ from oracle.models.prediction import PredictionCreate
 # can't estimate beyond this age
 AGE_UPPER_BOUND = 110
 
+LIFE_TABLE = {
+    "M": pd.read_pickle("data/canada_male_life_table.pkl"),
+    "F": pd.read_pickle("data/canada_male_life_table.pkl"),
+}
 
-def predict_life_expectancy(prediction: PredictionCreate) -> int:
+SMOKING_IMPACT_IN_MINUTES = {"M": -17, "F": -22}
+
+
+def _smoking_impact(prediction: PredictionCreate) -> float:
+    if prediction.smoking_daily_frequency is None:
+        return 0
+
+    days = 0
+
+    if prediction.smoking_start_date is not None and prediction.smoking_end_date is not None:
+        days = (prediction.smoking_end_date - prediction.smoking_start_date).days
+    elif prediction.smoking_start_date is not None:
+        days = (date.today() - prediction.smoking_start_date).days
+    return (SMOKING_IMPACT_IN_MINUTES[prediction.sex] * days * prediction.smoking_daily_frequency) / (365 * 60 * 24)
+
+
+def predict_life_expectancy(prediction: PredictionCreate) -> float:
     if prediction.age > AGE_UPPER_BOUND:
         return prediction.age
 
-    # get life table based on sex
-    life_table = (
-        pd.read_pickle("data/canada_male_life_table.pkl")
-        if prediction.sex == "M"
-        else pd.read_pickle("data/canada_female_life_table.pkl")
-    )
-
-    # determine life expectancy at the current age
-    life_expectancy_at_age = life_table.query("age == @prediction.age")["e_x"].item()
-
-    return int(prediction.age + life_expectancy_at_age)
+    expected_remaining_years = LIFE_TABLE[prediction.sex].query("age == @prediction.age")["e_x"].item()
+    life_expectancy = prediction.age + expected_remaining_years + _smoking_impact(prediction)
+    return round(life_expectancy, 2)

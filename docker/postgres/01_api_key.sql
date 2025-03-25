bcff635b6ae6b536135c0bc12b90bf98749aa778
Payam Yektamaram <payamyek@gmail.com>
2025-03-24 23:07:24 -0400

    feat: update local dev key

--- docker/postgres/01_api_key.sql
+++ docker/postgres/01_api_key.sql
@@ -1,3 +1,3 @@
 INSERT INTO public.apikey
 ("name", referer, client_id, hashed_api_key, salt, is_active, created_at)
-VALUES('local', 'http://localhost', 'c8bf2f0a-20f2-4f30-a234-a37d6fe9863f'::uuid, '0cb5bb56b5a273e9b0715c15b7277513767fa595df51288de6fba552cfe0470b', 'bab9b34e380a4617a555d5f13c0adb6a', true, '2025-03-25 01:35:58.574');
+VALUES('local', 'http://localhost:5173/', 'c8bf2f0a-20f2-4f30-a234-a37d6fe9863f'::uuid, '0cb5bb56b5a273e9b0715c15b7277513767fa595df51288de6fba552cfe0470b', 'bab9b34e380a4617a555d5f13c0adb6a', true, '2025-03-25 01:35:58.574');
